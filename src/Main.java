6371d71452b7b545feeb04349a37f3acfe46fae7
tornadusNEWB <nowaczek5@o2.pl>
2018-11-03 19:44:42 +0100

    2 tasks

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	KlasaTestowa klasaTestowa = new KlasaTestowa();
+
+
+        System.out.println(klasaTestowa.StringUpperCase("kot"));
+
+        klasaTestowa.listaLiczb();
+
+
+
+    }
+}
