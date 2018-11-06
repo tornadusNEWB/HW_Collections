341e3bb81f2629b8627b64c4db457a9b78e97b7c
tornadusNEWB <nowaczek5@o2.pl>
2018-11-06 22:17:52 +0100

    task 4/13

--- src/KlasaTestowa.java
+++ src/KlasaTestowa.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -9,7 +8,7 @@ public class KlasaTestowa {
      Metoda która przyjmuje ciąg znaków i zwraca ten sam ciąg znaków napisany samymi dużymi
      literami (patrz: metoda obiektu String - toUpperCase()), np. „Ala” zwróci: „ALA”.
     */
-    public String StringUpperCase(String ciagZnakow) {
+    public String stringUpperCase(String ciagZnakow) {
 
         return ciagZnakow.toUpperCase();
     }
@@ -17,33 +16,65 @@ public class KlasaTestowa {
     Metoda która przyjmuje listę liczb i zwraca największą z nich.
      */
 
-    public ArrayList<Integer> listaLiczb() {
-        ArrayList<Integer> dodajLiczbe = new ArrayList<>();
-        dodajLiczbe.add(77);
-        dodajLiczbe.add(12);
-        dodajLiczbe.add(13);
-        dodajLiczbe.add(2222);
-        dodajLiczbe.add(5);
-        dodajLiczbe.add(7779);
+    public List<Integer> listaLiczb(List<Integer> wartosci) {
+        int najwyszaLiczba = wartosci.get(0);
 
-        //System.out.println(Collections.max(dodajLiczbe));
-        //System.out.println(Collections.min(dodajLiczbe));
-
-        int najwyszaLiczba = dodajLiczbe.get(0);
-        int najmniejszaLiczba = dodajLiczbe.get(0);
-
-        for (int i = 0; i < dodajLiczbe.size(); i++) {
-            int liczba = dodajLiczbe.get(i);
+        for (int i = 0; i < wartosci.size(); i++) {
+            int liczba = wartosci.get(i);
             if (liczba > najwyszaLiczba) {
                 najwyszaLiczba = liczba;
             }
+        }
+        System.out.println("najwyzsza liczba to:");
+        return Collections.singletonList(najwyszaLiczba);
+    }
+
+    /*
+    Metodę która przyjmuje listę liczb i zwraca najmniejszą z nich.
+     */
+    public List<Integer> listaLiczbSecond(List<Integer> wartosci) {
+
+        int najmniejszaLiczba = wartosci.get(0);
+        for (int i = 0; i < wartosci.size(); i++) {
+            int liczba = wartosci.get(i);
             if (liczba < najmniejszaLiczba) {
                 najmniejszaLiczba = liczba;
             }
         }
-        System.out.println(najwyszaLiczba);
-        System.out.println(najmniejszaLiczba);
+        System.out.println("najmniejsza liczba to:");
+        return Collections.singletonList(najmniejszaLiczba);
+    }
 
-        return dodajLiczbe;
+    // Metodę która przyjmie listę liczb i zwróci listę tych które są podzielne przez 3 bez reszty.
+//    public List<Integer> listaLiczbThird(List<Integer> wartosci) {
+//
+//        for (Integer liczbyPodzielne : wartosci) {
+//            if (liczbyPodzielne % 3 == 0) {
+//                List<Integer> listaLiczbPodzielnychPrzez3 = new ArrayList<>();
+//                listaLiczbPodzielnychPrzez3.add(liczbyPodzielne);
+//                System.out.println(listaLiczbPodzielnychPrzez3);
+//            }
+//        }
+//        return null;
+//    }
+
+    /*
+    Zmodyfikuj poprzednią metodę tak aby przyjmowała listę liczb oraz cyfrę przez którą będziesz
+dzielił liczby z listy i zwrócisz te które są przez nie podzielne bez reszty. Np. getDivisible(lista,
+4) zwróci nową listę tych które były podzielne przez 4.
+     */
+    public List<Integer> listaLiczbThird(List<Integer> wartosci, int mianownik) {
+
+        for (Integer liczbyPodzielne : wartosci) {
+            if (liczbyPodzielne % mianownik == 0) {
+                List<Integer> listaLiczbPodzielnychPrzezMianownik = new ArrayList<>();
+                listaLiczbPodzielnychPrzezMianownik.add(liczbyPodzielne);
+                System.out.print("liczby podzielne podane przez mianownik uzytkownika");
+                System.out.println(listaLiczbPodzielnychPrzezMianownik);
+            }
+        }
+        return null;
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	KlasaTestowa klasaTestowa = new KlasaTestowa();
+        KlasaTestowa klasaTestowa = new KlasaTestowa();
+        System.out.println(klasaTestowa.stringUpperCase("kot"));
 
 
-        System.out.println(klasaTestowa.StringUpperCase("kot"));
 
-        klasaTestowa.listaLiczb();
+        List<Integer> wartosci = new ArrayList<>();
+        wartosci.add(100);
+        wartosci.add(66);
+        wartosci.add(33);
+        wartosci.add(-11);
+        wartosci.add(-55);
+        wartosci.add(55);
 
-
+        System.out.println(klasaTestowa.listaLiczb(wartosci));
+        System.out.println(klasaTestowa.listaLiczbSecond(wartosci));
+        System.out.println(klasaTestowa.listaLiczbThird(wartosci,5));
 
     }
 }
